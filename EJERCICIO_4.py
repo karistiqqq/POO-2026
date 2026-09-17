0ff247e61e73e6e0b8669b17517d709985d64c65
karistiqqq <karistizabalg@unal.edu.co>
2026-09-17 18:04:31 -0500

    EJERCICIO_4.py

--- EJERCICIO_4.py
+++ EJERCICIO_4.py
@@ -1,1 +1,21 @@
+#EJERCICIO 4
+class GrupoFamiliar:
+    def __init__(self, edad_juan):
+        self.juan = edad_juan
+        self._calcular_edades()
 
+    def _calcular_edades(self):
+        self.alberto = (2 * self.juan) / 3
+        self.ana = (4 * self.juan) / 3
+        self.mama = self.juan + self.alberto + self.ana
+
+    def edades(self):
+        print(f"La edad de Juan es: {self.juan}")
+        print(f"La edad de Alberto es: {self.alberto}")
+        print(f"La edad de Ana es: {self.ana}")
+        print(f"La edad de la mamá es: {self.mama}")
+
+
+edad= int(input("Ingrese la edad de Juan: "))
+familia = GrupoFamiliar(edad)
+familia.edades()
