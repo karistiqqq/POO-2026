9483cb75a740ab192dd862499eb12fb1d2c3e987
karistiqqq <karistizabalg@unal.edu.co>
2026-09-17 17:54:49 -0500

    EJERCICIO_14.py

--- EJERCICIO_14.py
+++ EJERCICIO_14.py
@@ -1,1 +1,20 @@
+#EJERCICIO 14
 
+class CalculadoraPotencias:
+    def __init__(self, valor):
+        self.valor = valor
+        self._obtener_potencias()
+
+    def _obtener_potencias(self):
+        self.al_cuadrado = self.valor ** 2
+        self.al_cubo = self.valor ** 3
+
+    def mostrar_resultados(self):
+        print(f"El número ingresado es: {self.valor}")
+        print(f"Su cuadrado es: {self.al_cuadrado}")
+        print(f"Su cubo es: {self.al_cubo}")
+
+#llamar función
+dato_ingreso = float(input("Ingrese un número: "))
+calc = CalculadoraPotencias(valor=dato_ingreso)
+calc.mostrar_resultados()
